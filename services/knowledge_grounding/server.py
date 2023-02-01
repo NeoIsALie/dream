e2cb8435992c3032a6e6cc61070c1ea303fa7606
Anastasiia Pokasova <apokasova@mymary.ru>
2023-02-01 14:02:08 +0300

    #1 use separate file for configs

--- services/knowledge_grounding/server.py
+++ services/knowledge_grounding/server.py
@@ -1,45 +1,10 @@
-import logging
-import os
 import random
 import time
 
 import sentry_sdk
-import torch
 from flask import Flask, request, jsonify
-from parlai.core.script import ParlaiPreloadModelScript
-from sentry_sdk.integrations.flask import FlaskIntegration
 
-sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
-
-
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
-cuda = torch.cuda.is_available()
-if cuda:
-    torch.cuda.set_device(0)  # singe gpu
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-
-kg_checkpoint_name = os.environ.get("MODEL_CKPT", "1_sent_48_epochs")
-
-logger.info(f"knowledge grounding model {kg_checkpoint_name} is set to run on {device}")
-
-kg_script = ParlaiPreloadModelScript.main(
-    task="topical_chat:generator",
-    init_model=f"/opt/conda/lib/python3.7/site-packages"
-    f"/data/models/topical_chat_blender90_{kg_checkpoint_name}/model.checkpoint",
-    model_file=f"/opt/conda/lib/python3.7/site-packages"
-    f"/data/models/topical_chat_blender90_{kg_checkpoint_name}/model",
-    fp16=False,
-    inference="nucleus",
-    topp=0.9,
-    skip_generation=False,
-)
-
-logger.info("knowledge grounding script has loaded the model and is ready")
+from config import *
 
 app = Flask(__name__)
 
